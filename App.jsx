c994920b0c0f251217a00de2732dd47fe844537b
hiruniMalshika <malshikahiruni99@gmail.com>
2021-05-05 10:18:33 +0530

    lab 07 part 2 submit

--- App.jsx
+++ App.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import PostListItem from "./components/PostListItem";
-import Posts from "./components/Posts";
-const posts = [
-    {
-        id: 1,
-        name: 'React',
-        description: 'Best UI library'
-    }, {
-        id: 2,
-        name: 'Node',
-        description: 'Server side JS'
-    }
-    ];
-export default class App extends React.Component {
+import React, {Component} from 'react';
+import {BrowserRouter  as Router, Route, Switch, Redirect} from 'react-router-dom';
+import PostHolder from "./components/PostHolder";
+export default class App extends Component {
 
 
     constructor(props) {
         super(props);
     }
     render() {
-        return <div>
-            <Posts posts = {posts}/>
-        </div>
+        return (
+            <Router>
+                <Switch>
+                    <Route  path='/posts'>
+                        <PostHolder/>
+                    </Route>
+                    <Redirect to='/posts'/>
+                </Switch>
+
+            </Router>
+
+            );
+
+
     }
 }
