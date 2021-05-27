013aca86fa814dcfc08fa81b6520a0f77eb59ce4
hiruniMalshika <malshikahiruni99@gmail.com>
2021-05-28 01:13:19 +0530

    lab 09

---
+++ src/main/java/com/af/project/controller/PostEndPoint.java
@@ -0,0 +1,34 @@
+package com.af.project.controller;
+
+import com.af.project.api.PostApi;
+import com.af.project.domain.Post;
+import com.af.project.dto.PostDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/posts")
+public class PostEndPoint {
+    private PostApi postApi;
+
+    @Autowired
+    public PostEndPoint(PostApi postApi){
+        this.postApi = postApi;
+    }
+    @GetMapping
+    public List<Post> getPosts(){
+        return postApi.getAllPosts();
+    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Post addPost(@RequestBody PostDto postDto){
+        Post post = new Post();
+        post.setName(postDto.getName());
+        post.setDescription(postDto.getDescription());
+        return postApi.addPost(post);
+    }
+
+}
